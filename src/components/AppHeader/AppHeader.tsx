155b25d7a347bb34edb7af9a9a42da7e99624bf1
Jik789 <stix.jik@gmail.com>
2023-05-24 13:40:47 +0300

    feat: header start styling

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,33 @@
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  BurgerIcon,
+  Logo,
+  ListIcon,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
+import styles from './AppHeader.module.css';
 
 const AppHeader = () => {
   return (
-    <div>
-      <BurgerIcon type="primary"></BurgerIcon>
-    </div>
+    <header className={styles.header}>
+      <div className={styles.leftSection}>
+        <div className={styles.menuItem}>
+          <BurgerIcon type="primary"></BurgerIcon>
+          <p className={styles.mainText}>Конструктор</p>
+        </div>
+        <div className={styles.menuItem}>
+          <ListIcon type="secondary" />
+          <p className={styles.mainTextDefault}>Лента заказов</p>
+        </div>
+      </div>
+      <div className={styles.menuLogo}>
+        <Logo />
+      </div>
+      <div className={styles.menuItem}>
+        <ProfileIcon type="secondary" />
+        <p className={styles.mainTextDefault}>Личный кабинет</p>
+      </div>
+    </header>
   );
 };
 
