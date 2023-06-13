d75313ef8c88d929b033553ba7d9899aaeff6991
Jik789 <stix.jik@gmail.com>
2023-06-13 13:06:13 +0300

    feat: total price

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,13 +1,29 @@
 import { Button, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import ConstructorList from './ConstructorList/ConstructorList';
 import styles from './BurgerConstructor.module.css';
 import Modal from '../Modal/Modal';
 import OrderDetails from '../OrderDetails/OrderDetails';
 import { OREDER_MOCK_DATA } from '../../utils/const';
+import { useAppSelector } from '../../store/store';
+import { IExtData } from '../../utils/interfaces';
 
 const BurgerConstructor = () => {
   const [constructorInModal, setIngredientInModal] = useState(false);
+  const burgersData = useAppSelector((state) => state.allIngredientsCurrentBurger);
+
+  const totalPrice = useMemo(() => {
+    const bun = burgersData.bun;
+    const ingredients = burgersData.ingredients;
+
+    const bunPrice = bun ? bun.price * 2 : 0;
+    const ingredientsPrice = ingredients.reduce(
+      (acc: number, item: IExtData) => acc + item.price,
+      0
+    );
+
+    return bunPrice + ingredientsPrice;
+  }, [burgersData]);
 
   const closeConstructorModal = () => {
     setIngredientInModal(false);
@@ -23,7 +39,7 @@ const BurgerConstructor = () => {
         <ConstructorList />
         <div className={styles.orderInfo}>
           <div className={styles.orederPrice}>
-            <p className={styles.mainTextDefault}>12345</p>
+            <p className={styles.mainTextDefault}>{totalPrice}</p>
             <CurrencyIcon type="primary" />
           </div>
           <Button htmlType="button" type="primary" size="medium" onClick={openConstructorModal}>
